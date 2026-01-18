9bb076a252eddb521b94e2180b19830ec68db4cd
Phakphum Pinyodom <phakphum.pinyodom@gmail.com>
2026-01-18 19:53:21 +0700

    fix: update new basePath = "/my-resume"

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  basePath: "/my-resume/",
+  basePath: "/my-resume",
   /* config options here */
 };
 
